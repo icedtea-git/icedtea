15f4190ff78199bb0e0e65d957a34a8f45ea2c6d
Lillian Angel <langel@redhat.com>
2008-03-27 09:52:39 -0400

    2008-03-27  Lillian Angel  <langel@redhat.com>
            * Makefile.am
            (clean-patch): Fixed so patches are actually reverted.
            * Makefile.in: Regenerated.

--- generated/sun/awt/X11/generator/sizer.32.c
+++ generated/sun/awt/X11/generator/sizer.32.c
@@ -11,20 +11,22 @@
 #include "color.h"
 #include "colordata.h"
 
-/* the struct below was copied from MwmUtil.h to workaround a lesstif bug:
-   http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6586752 */
+typedef struct
 
-typedef struct PROPMOTIFWMHINTS {
-/* 32-bit property items are stored as long on the client (whether
- * that means 32 bits or 64).  XChangeProperty handles the conversion
- * to the actual 32-bit quantities sent to the server.
- */
-    unsigned long   flags;
-    unsigned long   functions;
-    unsigned long   decorations;
-    long            inputMode;
-    unsigned long   status;
+{
+
+    unsigned long flags;
+
+    unsigned long functions;
+
+    unsigned long decorations;
+
+    long inputMode;
+
+    unsigned long status;
+
 } PropMwmHints;
+
 
 
 int main(){
