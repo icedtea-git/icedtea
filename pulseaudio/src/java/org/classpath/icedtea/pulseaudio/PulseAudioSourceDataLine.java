10c385c6c29731dbdf194ed0e6cf3eea2f256cdb
Omair Majid <omajid@redhat.com>
2012-06-28 19:45:23 -0400

    Synchronize access to shared variable
    The variable writeInterrupted is accessed everywhere while holding a lock on
    'this' object, except in one location. Fix that.
    
    2012-06-28  Omair Majid  <omajid@redhat.com>
    
        * pulseaudio/src/java/org/classpath/icedtea/pulseaudio/PulseAudioSourceDataLine.java
        (write): Synchronize access to writeInterrupted flag.

--- pulseaudio/src/java/org/classpath/icedtea/pulseaudio/PulseAudioSourceDataLine.java
+++ pulseaudio/src/java/org/classpath/icedtea/pulseaudio/PulseAudioSourceDataLine.java
@@ -179,8 +179,10 @@ public final class PulseAudioSourceDataLine extends PulseAudioDataLine
             synchronized (eventLoop.threadLock) {
 
                 do {
-                    if (writeInterrupted) {
-                        return sizeWritten;
+                    synchronized (this) {
+                        if (writeInterrupted) {
+                            return sizeWritten;
+                        }
                     }
 
                     if (availableSize == -1) {
