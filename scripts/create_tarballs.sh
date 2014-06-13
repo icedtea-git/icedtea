89e2254b03f633c8b95a6555c5b1e12e889d3d38
Andrew John Hughes <gnu_andrew@member.fsf.org>
2014-06-13 01:46:35 +0100

    Update tarball scripts and .hgignore
    2014-06-12  Andrew John Hughes  <gnu.andrew@member.fsf.org>
    
    	* .hgignore: Add compile script added by
    	recent version of Automake.
    	* scripts/create_tarballs.sh: New script
    	to create tarballs using Mercurial, as used
    	for the drops created for the previous bump.
    	* scripts/gen_changeset_and_sha256sums.sh:
    	Updated to latest local version, with support
    	for tarballs created by create_tarballs.sh.
    	* scripts/update_tarballs.sh:
    	Updated to latest local version with support
    	for OpenJDK 8.

--- scripts/create_tarballs.sh
+++ scripts/create_tarballs.sh
@@ -16,12 +16,13 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-DOWNLOAD_DIR=$1
-URL=$2
+CHECKOUT_DIR=$1
+DOWNLOAD_DIR=$2
 TAG=$3
-CHECKOUT_DIR=$4
 RUNNING_DIR=$(dirname $0)
-echo $PWD
+CTYPE=bz2
+echo "PWD: $PWD"
+echo "COMPRESSION TYPE: ${CTYPE}"
 
 if [ $(echo $0|grep '8$') ]; then
     echo "Assuming OpenJDK 8 and later";
@@ -32,19 +33,17 @@ else
     OPENJDK8=false;
 fi
 
+if test "x$CHECKOUT_DIR" = "x"; then
+    echo "ERROR: Checkout directory must be specified";
+    echo "$0 <CHECKOUT_DIR> <DOWNLOAD_DIR> <TAG>"
+    exit -1;
+fi
+
 if test "x$DOWNLOAD_DIR" = "x"; then
     if test "x$OPENJDK8" = "xfalse"; then
       DOWNLOAD_DIR=/home/downloads/java/drops/icedtea7 ;
     else
       DOWNLOAD_DIR=/home/downloads/java/drops/icedtea8 ;
-    fi
-fi
-
-if test "x$URL" = "x"; then
-    if test "x$OPENJDK8" = "xfalse"; then
-      URL=http://icedtea.classpath.org/hg/icedtea7-forest ;
-    else
-      URL=http://icedtea.classpath.org/hg/icedtea8-forest ;
     fi
 fi
 
@@ -57,29 +56,38 @@ elif echo ${TAG}|grep '^icedtea' ; then
     fi
 fi
 
-echo "URL = $URL, TAG = $TAG";
-echo Downloading new tarballs to $DOWNLOAD_DIR
+echo "TAG = $TAG";
+echo "Creating new tarballs in $DOWNLOAD_DIR"
+echo "Using checkout directory $CHECKOUT_DIR"
+URL=$(hg -R ${CHECKOUT_DIR} paths default)
+PREFIX=$(echo ${URL}|sed -r 's#.*/([^/]*)$#\1#'|sed 's#\.#-#')
+echo "Upstream URL is ${URL}"
+echo "Prefix for root tree is ${PREFIX}"
 pushd $DOWNLOAD_DIR
 for repos in . corba jaxp jaxws langtools hotspot jdk $NASHORN;
 do
     if test "x$repos" = "x."; then
 	FILENAME=openjdk;
+	REPONAME=${PREFIX};
     else
-	FILENAME=$repos;
+	FILENAME=${repos};
+	REPONAME=${repos};
     fi
-    if echo ${TAG} | egrep '^(icedtea|jdk)' > /dev/null ; then
+    if echo ${TAG} | egrep '^(icedtea|jdk|tip)' > /dev/null ; then
 	CHANGESET=$(hg log -r ${TAG} -R ${CHECKOUT_DIR}/$repos | head -n1| awk -F ':' '{print $3}')
     else
 	CHANGESET=${TAG}
     fi
-    wget -O ${FILENAME}.tar.gz ${URL}/$repos/archive/${CHANGESET}.tar.gz
+    echo "Creating ${FILENAME}.tar.${CTYPE} containing ${REPONAME}-${CHANGESET} from tag ${TAG} in ${CHECKOUT_DIR}/${repos}"
+    hg archive -R ${CHECKOUT_DIR}/$repos -t t${CTYPE} -r ${TAG} -p ${REPONAME}-${CHANGESET} ${FILENAME}.tar.${CTYPE}
 done
 echo Removing outdated symlinks
 find $DOWNLOAD_DIR -maxdepth 1 -type l -exec rm -vf '{}' ';'
 popd
 echo Generating new changeset IDs and SHA256 sums
+echo URL = ${URL}
 if test "x$OPENJDK8" = "xfalse"; then
-  $RUNNING_DIR/gen_changeset_and_sha256sums.sh gz $DOWNLOAD_DIR ${URL}
+  $RUNNING_DIR/gen_changeset_and_sha256sums.sh bz2 $DOWNLOAD_DIR ${URL}
 else
-  $RUNNING_DIR/gen_changeset_and_sha256sums_8.sh gz $DOWNLOAD_DIR ${URL}
+  $RUNNING_DIR/gen_changeset_and_sha256sums_8.sh bz2 $DOWNLOAD_DIR ${URL}
 fi
--- scripts/gen_changeset_and_sha256sums.sh
+++ scripts/gen_changeset_and_sha256sums.sh
@@ -1,19 +1,79 @@
 #!/bin/bash
 
-DOWNLOAD_DIR=$1
+# Copyright (C) 2014 Red Hat, Inc.
+# Written by Andrew John Hughes <gnu.andrew@redhat.com>.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+COMPRESSION_TYPE=$1
+DOWNLOAD_DIR=$2
+ROOT_URL=$3
+
+if [ $(echo $0|grep '_8') ]; then
+    echo "Assuming OpenJDK 8 and later";
+    OPENJDK8=true;
+    NASHORN=nashorn;
+else
+    echo "Assuming OpenJDK 7 and earlier";
+    OPENJDK8=false;
+fi
+
+if test "x$COMPRESSION_TYPE" = "x"; then
+    echo "ERROR: Compression type must be specified.";
+    echo "$0 <COMPRESSION_TYPE> <DOWNLOAD_DIR> <ROOT_URL>"
+    exit -1;
+fi
 
 if test "x$DOWNLOAD_DIR" = "x"; then
-    DOWNLOAD_DIR=/home/downloads/java/icedtea ;
+    if test "x$OPENJDK8" = "xfalse"; then
+      DOWNLOAD_DIR=/home/downloads/java/drops/icedtea7 ;
+    else
+      DOWNLOAD_DIR=/home/downloads/java/drops/icedtea8 ;
+    fi
+fi
+
+if test "x$URL" = "x"; then
+    if test "x$OPENJDK8" = "xfalse"; then
+      URL=http://icedtea.classpath.org/hg/icedtea7-forest ;
+    else
+      URL=http://icedtea.classpath.org/hg/icedtea8-forest ;
+    fi
 fi
 
 rm -f /tmp/changesets /tmp/sums
-for repos in corba hotspot jaxp jaxws jdk langtools openjdk
+for repos in corba jaxp jaxws jdk langtools openjdk $NASHORN
 do
-    echo Generating changeset and checksum for $repos using $DOWNLOAD_DIR/$repos.tar.gz
-    file=$DOWNLOAD_DIR/$repos.tar.gz
-    id=$(echo $repos|tr '[a-z]' '[A-Z]')
-    sha256sum=$(sha256sum $file|awk '{print $1}')
-    changeset=$(tar tzf $file|head -n1|sed -r "s#[a-z0-9-]*-([0-9a-z]*)/.*#\1#")
-    echo "${id}_CHANGESET = $changeset" >> /tmp/changesets
-    echo "${id}_SHA256SUM = $sha256sum" >> /tmp/sums
+    file=$DOWNLOAD_DIR/$repos.tar.${COMPRESSION_TYPE}
+    echo Generating changeset and checksum for $repos using ${file}
+    if [ -e $file ] ; then
+      id=$(echo $repos|tr '[a-z]' '[A-Z]')
+      sha256sum=$(sha256sum $file|awk '{print $1}')
+      changeset=$(tar tf $file|head -n1|sed -r "s#[a-z0-9-]*-([0-9a-z]*)/.*#\1#")
+      name=$(echo ${DOWNLOAD_DIR}|sed -r 's#.*(icedtea.*)#\1#'|sed 's#7/#-#')
+      ln -sf ${file} ${DOWNLOAD_DIR}/${name}-${repos}-${changeset}.tar.${COMPRESSION_TYPE}
+      echo "${id}_CHANGESET = $changeset" >> /tmp/changesets
+      echo "${id}_SHA256SUM = $sha256sum" >> /tmp/sums
+    fi
 done
+
+file=${DOWNLOAD_DIR}/hotspot.tar.${COMPRESSION_TYPE}
+echo Generating changeset and checksum for hotspot using ${file}
+if [ -e ${file} ] ; then
+  sha256sum=$(sha256sum $file|awk '{print $1}')
+  name=$(echo ${DOWNLOAD_DIR}|sed -r 's#.*(icedtea.*)#\1#'|sed 's#7/#-#')
+  changeset=$(tar tf $file|head -n1|sed -r "s#[a-z0-9-]*-([0-9a-z]*)/.*#\1#")
+  ln -sf ${file} ${DOWNLOAD_DIR}/${name}-hotspot-$changeset.tar.${COMPRESSION_TYPE}
+  echo "default ${ROOT_URL}/hotspot ${changeset} ${sha256sum}" > /tmp/hotspot.map
+fi
+    
